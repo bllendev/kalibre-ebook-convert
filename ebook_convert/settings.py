5bc71837530ef6cb01efa8280b391dc6e0ff1b2a
bllen <bllendev@gmail.com>
2023-11-08 18:32:09 -0700

    update

--- ebook_convert/settings.py
+++ ebook_convert/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'postgres'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
